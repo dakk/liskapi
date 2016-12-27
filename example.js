5376d429eeff83bf4d3885e0b7ba28baa231c6f1
andreafspeziale <andreaf.speziale@gmail.com>
2016-12-27 15:08:50 +0100

    upgrade of example.js - readme updated with basic usage of param in a get request - get request tested and fixed - added asserts require

--- example.js
+++ example.js
@@ -8,8 +8,17 @@ const lisk = require ('liskapi')(params);
 
 lisk.getSyncStatus ().call ()
     .then ((res) => {
-        console.log (`Got data ${JSON.stringify (res)}`);
+        console.log (`Get sync status data\n ${JSON.stringify (res)}`);
     })
     .catch ((err) => {
         console.log ('Got an error', err);
+    });
+
+lisk.getBalance ({address: '13968139166426148658L'})
+    .call ()
+    .then ((res) => {
+        console.log (`Get balance of Max delegate\n ${JSON.stringify (res)}`);
+    })
+    .catch ((err) => {
+        console.log ('Got an error in getting balance\n', err);
     });
